d322330ed62edb52d7e8fee1bba2dbb0bdd75430
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2018-09-13 17:36:39 +0200

    add get heroes service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core'
 import {Hero} from '../hero'
-import {HEROES} from '../mock-heroes'
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -14,7 +15,7 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   }
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
@@ -22,10 +23,17 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero
   }
 
-  constructor() {
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
+
+  constructor(private heroService: HeroService) {
+  }
+
+
   ngOnInit() {
+    this.getHeroes()
   }
 
 }
